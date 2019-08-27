e6e8c205b97c0d912fd36339ffa8f26c8e6ef840
David Choi <david.choi@JEMA-000101.local>
2019-08-27 12:15:55 -0700

    step 6: Support 1 custom delimiter of one character length

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -7,15 +7,14 @@ public class StringCalculator
 
 	private static double calculate(string input, string optr, Func<double, double, double> lambda)
 	{
-
-		string[] delimiter = new string[]{"\n", ","};
-
+		//separates delimiter(s) from number
+		Tuple<string[], string> tuple = separateDelimiterFromNumber(input);
+		string[] delimiter = tuple.Item1;
+		string numbers = tuple.Item2;
 		//separates string of numbers using given delimiter
-		List<string> parsedNumbers = parseUsingDelimiter(input, delimiter);
-
+		List<string> parsedNumbers = parseUsingDelimiter(numbers, delimiter);
 		double result = -1;
 		bool isFirstItem = true;
-
 		foreach (string stringNumber in parsedNumbers)
 		{
 			double number = convertToNumber(stringNumber);
@@ -31,6 +30,37 @@ public class StringCalculator
 		}
 
 		return result;
+	}
+
+
+	private static Tuple<string[], string> separateDelimiterFromNumber(string input)
+	{
+		string[] defaultDelimiter = new string[]{"\n", ","};
+		string numbers = input;
+		//check if the input contains a custom delimiter
+		if (input.Length > 1 && input.Substring(0, 2) == "//")
+		{
+			//split on "\n" to separate delimiters & number --> [[{delimiter1}]...[{delimiterN}]], {numbers}]
+			List<string> inputArray = new List<string>(input.Substring(2).Split(new[]{'\n'}, 2));
+			//split on on brackets to separate delimiters --> [{delimiter1}, {delimiter2},...{delimiterN}]
+			string[] customDelimiter = inputArray[0].Split(new string[]{"[", "]"}, StringSplitOptions.RemoveEmptyEntries);
+			//combine default delimiters & custom delimiters
+			List<string> list = new List<string>();
+			list.AddRange(customDelimiter);
+			list.AddRange(defaultDelimiter);
+			string[] delimiter = list.ToArray();
+			
+			if (inputArray.Count == 1) {
+				numbers = "";
+			}
+			else
+			{
+				numbers = inputArray[1];
+			}
+			return new Tuple<string[], string>(delimiter, numbers);
+		}
+
+		return new Tuple<string[], string>(defaultDelimiter, numbers);
 	}
 
 
@@ -86,9 +116,16 @@ public class StringCalculator
 		Console.WriteLine(add("1\n2,3"));
 		Console.WriteLine(add("1\n2\n3"));
 
-		//step 3 unit tests
-		Console.WriteLine(add("-12,3"));
-		Console.WriteLine(add("-1\n2\n3"));
+		//step 4 unit tests
+		Console.WriteLine(add("-2,6"));
+		Console.WriteLine(add("-6"));
+
+		//step 5 unit tests
+		Console.WriteLine(add("2,1001,6"));
+		Console.WriteLine(add("1000,1001,6"));
+
+		//step 6 unit tests
+		Console.WriteLine(add("//;\n2;5"));
 	}
 
 }
